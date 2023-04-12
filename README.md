d66ead697168d368decf2ffdc60d95a283be2b0a
Kous92 <kous92@gmail.com>
2023-04-12 12:14:46 +0200

    Initialisation repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Super News iOS V2 (UIKit)
---
+++ SuperNewsV2UIKit/SuperNewsV2UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperNewsV2UIKit
+//
+//  Created by Koussaïla Ben Mamar on 12/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
