e7b7ed29ba79c7635e188cd71ac29caad4dc3f30
Kous92 <kous92@gmail.com>
2023-04-17 13:05:47 +0200

    Mise en place du service réseau avec NewsAPI

--- SuperNewsV2UIKit/SuperNewsV2UIKit/ViewController.swift
+++ SuperNewsV2UIKit/SuperNewsV2UIKit/ViewController.swift
@@ -7,14 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var dataAPI: SuperNewsDataAPIService?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .darkGray
+        dataAPI = SuperNewsNetworkAPIService()
+        
+        Task {
+            await testFetchSources()
+            await testFetchArticles()
+        }
     }
-
-
+    
+    func testFetchSources() async {
+        guard let dataAPI = dataAPI as? SuperNewsNetworkAPIService else {
+            return
+        }
+        
+        let result = await dataAPI.fetchAllNewsSources()
+        let result1 = await dataAPI.fetchNewsSources(country: "fr")
+        let result2 = await dataAPI.fetchNewsSources(category: "business")
+        let result3 = await dataAPI.fetchNewsSources(language: "en")
+        
+        print("Toutes les sources:\n\(result)\n")
+        print("Sources de France:\n\(result1)\n")
+        print("Sources business:\n\(result2)\n")
+        print("Sources en anglais:\n\(result3)\n")
+    }
+    
+    func testFetchArticles() async {
+        guard let dataAPI = dataAPI as? SuperNewsNetworkAPIService else {
+            return
+        }
+        
+        let result = await dataAPI.fetchTopHeadlinesNews(countryCode: "fr", category: nil)
+        let result1 = await dataAPI.fetchTopHeadlinesNews(countryCode: "us", category: "technology")
+        let result2 = await dataAPI.fetchTopHeadlinesNews(sourceName: "abc-news")
+        let result3 = await dataAPI.searchNewsFromEverything(with: "PSG", language: "fr", sortBy: "relevancy")
+        
+        print("Actualités en France:\n\(result)\n")
+        print("Actualités tech aux USA:\n\(result1)\n")
+        print("Actualités d'ABC News:\n\(result2)\n")
+        print("Recherche de PSG en français:\n\(result3)\n")
+    }
 }
 
