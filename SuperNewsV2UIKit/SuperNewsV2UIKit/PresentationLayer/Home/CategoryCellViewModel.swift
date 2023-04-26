90491df6e717df2df937a895a49dea33d2c0fcc7
Kous92 <kous92@gmail.com>
2023-04-26 16:51:38 +0200

    Ajout des catégories d'actualités et optimisations UI

---
+++ SuperNewsV2UIKit/SuperNewsV2UIKit/PresentationLayer/Home/CategoryCellViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryCellViewModel.swift
+//  SuperNewsV2UIKit
+//
+//  Created by Koussaïla Ben Mamar on 26/04/2023.
+//
+
+import Foundation
+
+struct CategoryCellViewModel {
+    let title: String
+    let categoryId: String
+    
+    init(title: String, categoryId: String) {
+        self.title = title
+        self.categoryId = categoryId
+    }
+}
+
+extension CategoryCellViewModel {
+    static func getCategories() -> [CategoryCellViewModel] {
+        return [
+            CategoryCellViewModel(title: "Business", categoryId: "business"),
+            CategoryCellViewModel(title: "Divertissement", categoryId: "entertainment"),
+            CategoryCellViewModel(title: "Général", categoryId: "general"),
+            CategoryCellViewModel(title: "Santé", categoryId: "health"),
+            CategoryCellViewModel(title: "Science", categoryId: "science"),
+            CategoryCellViewModel(title: "Sports", categoryId: "sports"),
+            CategoryCellViewModel(title: "Technologie", categoryId: "technology")
+        ]
+    }
+}
