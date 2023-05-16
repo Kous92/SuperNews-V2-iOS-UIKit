5e98c5c1d7a34083f6037d24e654dad84dc86807
Kous92 <kous92@gmail.com>
2023-05-16 20:00:59 +0200

    Squelette UI de SearchViewController

--- SuperNewsV2UIKit/SuperNewsV2UIKit/PresentationLayer/Search/SearchViewController.swift
+++ SuperNewsV2UIKit/SuperNewsV2UIKit/PresentationLayer/Search/SearchViewController.swift
@@ -6,10 +6,79 @@
 //
 
 import UIKit
+import SnapKit
+import Combine
 
 final class SearchViewController: UIViewController {
 
     weak var coordinator: SearchViewControllerDelegate?
+    
+    // Background
+    lazy var backgroundGradient: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        let blue = UIColor(named: "SuperNewsBlue")?.cgColor ?? UIColor.blue.cgColor
+        let darkBlue = UIColor(named: "SuperNewsDarkBlue")?.cgColor ?? UIColor.black.cgColor
+        gradient.type = .axial
+        gradient.colors = [
+            blue,
+            darkBlue,
+            darkBlue,
+            UIColor.black.cgColor
+        ]
+        gradient.locations = [0, 0.25, 0.5, 1]
+        return gradient
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "customCell")
+        tableView.rowHeight = UIScreen.main.bounds.width * (9 / 16)
+        tableView.estimatedRowHeight = UIScreen.main.bounds.width * (9 / 16)
+        tableView.separatorStyle = .none
+        tableView.isHidden = true
+        tableView.backgroundColor = .clear
+        
+        return tableView
+    }()
+    
+    // For searching news
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Rechercher"
+        searchBar.backgroundImage = UIImage()
+        searchBar.showsCancelButton = false
+        searchBar.delegate = self
+        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).title = "Annuler"
+        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = .white
+        return searchBar
+    }()
+    
+    private lazy var loadingSpinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.style = .medium
+        spinner.transform = CGAffineTransform(scaleX: 2, y: 2)
+        spinner.hidesWhenStopped = true
+        
+        return spinner
+    }()
+    
+    private lazy var noResultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.minimumScaleFactor = 0.5
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.setShadowLabel(string: "Aucun article disponible", font: UIFont.systemFont(ofSize: Constants.TopHeadlines.noResultLabelFontSize, weight: .medium), textColor: .white, shadowColor: .blue, radius: 3)
+        label.isHidden = true
+        
+        return label
+    }()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -21,8 +90,131 @@ final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
-        // Do any additional setup after loading the view.
+        
+        setNavigationBar()
+        setViewBackground()
+        buildViewHierarchy()
+        setConstraints()
+        setBindings()
+    }
+    
+    private func buildViewHierarchy() {
+        view.addSubview(loadingSpinner)
+        view.addSubview(noResultLabel)
+        view.addSubview(tableView)
+        view.addSubview(searchBar)
+    }
+    
+    private func setConstraints() {
+        loadingSpinner.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        noResultLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(Constants.TopHeadlines.horizontalMargin)
+        }
+        
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.bottom.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+        }
+    }
+    
+    private func setBindings() {
+        
+    }
+}
+
+extension SearchViewController {
+    private func setNavigationBar() {
+        navigationItem.title = tabBarController?.tabBar.items?[1].title
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    private func setViewBackground() {
+        backgroundGradient.frame = view.bounds
+        view.layer.addSublayer(backgroundGradient)
+    }
+    
+    private func hideTableView() {
+        tableView.isHidden = true
+    }
+    
+    private func displayNoResult() {
+        tableView.isHidden = true
+        noResultLabel.isHidden = false
+    }
+    
+    private func updateTableView() {
+        tableView.reloadData()
+        tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+        tableView.isHidden = false
+    }
+    
+    private func setLoadingSpinner(isLoading: Bool) {
+        if isLoading {
+            loadingSpinner.startAnimating()
+        } else {
+            loadingSpinner.stopAnimating()
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return viewModel?.numberOfRowsInTableView() ?? 0
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? NewsTableViewCell, let cellViewModel = viewModel?.getNewsCellViewModel(at: indexPath) else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(with: cellViewModel)
+        cell.backgroundColor = .clear
+        cell.backgroundView = UIView()
+        cell.selectedBackgroundView = UIView()
+        
+        return cell
+         */
+        return UITableViewCell()
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // viewModel?.goToArticleDetailView(selectedViewModelIndex: indexPath.row)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // viewModel?.searchQuery = searchText
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // viewModel?.searchQuery = ""
+        self.searchBar.text = ""
+        self.searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.setShowsCancelButton(false, animated: true)
     }
 }
 
