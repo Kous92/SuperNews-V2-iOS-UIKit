129b26ca20c4c5889f71823814b20f96d847796d
Kous92 <kous92@gmail.com>
2023-12-10 00:48:00 +0100

    Ajout de Firebase et de la configuration Crashlytics

--- SuperNewsV2UIKit/SuperNewsV2UIKit/EntryPoint/AppDelegate.swift
+++ SuperNewsV2UIKit/SuperNewsV2UIKit/EntryPoint/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Enable Firebase services (Crashlytics)
+        FirebaseApp.configure()
+        
         return true
     }
 
